370ae6ed86df6c1c7d7953f478cad4f464adfc8d
feudor2 <feudor987@mail.ru>
2026-01-21 00:04:03 +0300

    Fix import

--- app/run_api.py
+++ app/run_api.py
@@ -5,7 +5,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "api:app",
+        "app.api:app",
         host="0.0.0.0",
         port=8500,
         workers=1,  # Use 1 worker since we're using async
